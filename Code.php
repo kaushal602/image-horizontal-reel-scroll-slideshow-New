98e0d3a65ffb682b87ab79f5c50942105629bcab
kaushal602 <32062745+kaushal602@users.noreply.github.com>
2018-05-16 11:02:53 +0530

    Update Code.php

--- Code.php
+++ Code.php
@@ -10,3 +10,19 @@ Data Get from DataBase
       <img alt="Education" width="100%" src="<?php echo $res->Ihrss_path; ?>" >
   </li>
 <?php } ?>
+
+
+
+Photo Gallery
+<?php
+  global $wpdb;
+  $result = $wpdb->get_results("SELECT * FROM `wp_bwg_image` order By `order` ASC ");
+?>
+
+<?php foreach($result as $res){ ?>
+  <div class="item">
+    <div class="gallery-img-box" data-src="<?php echo site_url().'/wp-content/uploads/photo-gallery'.$res->image_url; ?>">
+        <a href="#"><figure><img src="<?php echo site_url().'/wp-content/uploads/photo-gallery'.$res->thumb_url; ?>" alt="<?php echo $res->alt; ?>"></figure></a>
+      </div>
+  </div>
+<?php } ?>
